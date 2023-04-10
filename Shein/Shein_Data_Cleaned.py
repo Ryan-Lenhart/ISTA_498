31b11a143bc50ef0127f60beea6adf7c4dd68171
RyanLenhart <129101478+RyanLenhart@users.noreply.github.com>
2023-04-09 21:02:03 -0700

    Updated Files From Drive
    Put all files from drive into folders and any extra documents

---
+++ Shein/Shein_Data_Cleaned.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np 
+
+
+file_path = "C:\Users\Braydon\Desktop\Shein_Data\Pivot Data\Shein_Pivot.csv"
+
+shein_df = pd.read_csv("file_path")
+
+print(shein_df.head())
